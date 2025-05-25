7d5f17679ec346d56c7a3085e26806df8c241ca9
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-05-25 10:43:36 +0300

    implements stack

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,51 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
+        
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
 
     def peek(self):
-        pass
+        if not self.isEmpty():
+            return self.items[-1]
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
 
     def search(self, target):
-        pass
+        try:
+            return len(self.items) - self.items.index(target) - 1
+        except ValueError:
+            return -1
+    
+
+# stack = Stack(limit=4)
+
+# stack.push(10)
+# stack.push(20)
+# stack.push(30)
+# stack.push(40)
+
+# print("Current stack items:", stack.items)
+# print("Top of stack:", stack.peek())
+# print("Stack size:", stack.size())
+# print("Is the stack full?", stack.full())
+# print("Is the stack empty?", stack.isEmpty())
+# print("Search 300", stack.search(300))
+
+# for item in stack.items:
+#     print(item)
